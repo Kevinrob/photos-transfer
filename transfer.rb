664e6ad003371a43a490c89ab1f2bf926787b18b
Kevin Robatel <kevinrob2@gmail.com>
2017-02-12 10:36:40 +0100

    Add project files

---
+++ transfer.rb
@@ -0,0 +1,38 @@
+require 'exifr'
+require 'fileutils'
+
+path = 'G:\DCIM\101D7000'
+destination = '\\\\192.168.1.100\Multimedia\Photos\\'
+
+count = 0
+Dir.foreach(path) do |item|
+  next if item == '.' or item == '..'
+  # do work on real items
+
+  count += 1
+  file_path = path + "\\" + item
+  print file_path + '... '
+
+  begin
+    exif = EXIFR::TIFF.new(file_path)
+    date = exif.date_time
+
+    dir = date.strftime("%Y-%m-%d")
+    destination_folder = destination + dir + '\\'
+
+    print '-> ' + destination_folder + item
+
+    if File.exist?(destination_folder + item)
+      print ' Already exist!'
+    else
+      FileUtils.mkdir_p(destination_folder)
+      FileUtils.cp(file_path,destination_folder + item)
+    end
+  rescue Exception
+    print 'Error with ' + file_path
+  end
+
+  print "\n"
+end
+
+print "#{count} images"
